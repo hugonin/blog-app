c3740e64f1439bf3382870b1dcf45f5f7ba05466
Hugonin <jeanhugues.doumbe@gmail.com>
2022-10-12 23:22:11 +0200

    Prepare to deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const path = require("path")
 const express = require("express");
 const cors = require("cors");
 
@@ -19,13 +20,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(errorHandler);
+
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/articles", require("./routes/articleRoutes"));
 
-app.get("/", function (req, res) {
-  res.status(200).json({ message: "Bienvenue sur le Blog" });
-});
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../client/build")))
+
+  app.get("*", (req,res) => res.sendFile(__dirname, "../", "client", "build", "index.html"))
+}  else {
+  app.get("/", function (req, res) {
+    res.status(200).json({ message: "Bienvenue sur le Blog" });
+  });
+
+}
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
